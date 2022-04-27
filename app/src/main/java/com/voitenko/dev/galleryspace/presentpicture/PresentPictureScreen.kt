2df7f25c72a0f346cd0f01eafbd64c947d00201c
maxvoitenko <voitenko.dev@gmail.com>
2022-04-27 23:22:48 +0300

    add scroll

--- app/src/main/java/com/voitenko/dev/galleryspace/presentpicture/PresentPictureScreen.kt
+++ app/src/main/java/com/voitenko/dev/galleryspace/presentpicture/PresentPictureScreen.kt
@@ -1,9 +1,12 @@
 package com.voitenko.dev.galleryspace.presentpicture
 
+import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
@@ -12,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
@@ -23,6 +27,7 @@ import com.voitenko.dev.galleryspace.designsystem.*
 import com.voitenko.dev.galleryspace.designsystem.components.*
 import com.voitenko.dev.galleryspace.designsystem.modifiers.neu
 import com.voitenko.dev.galleryspace.designsystem.modifiers.shimmer
+import kotlin.math.min
 
 @ExperimentalFoundationApi
 @ExperimentalComposeUiApi
@@ -34,19 +39,35 @@ fun PresentPictureScreen() {
             .data("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/%D0%A7%D1%91%D1%80%D0%BD%D1%8B%D0%B9_%D1%81%D1%83%D0%BF%D1%80%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82._1915._%D0%93%D0%A2%D0%93.png/350px-%D0%A7%D1%91%D1%80%D0%BD%D1%8B%D0%B9_%D1%81%D1%83%D0%BF%D1%80%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82._1915._%D0%93%D0%A2%D0%93.png".toUri())
             .size(Size.ORIGINAL).build()
     )
+    val title = "Van Gogh"
+    val description =
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam."
+    val sign = "by Maximilian II"
+
+    val scrollState = rememberLazyListState()
+
+    val scrollOffset = min(1f, (scrollState.firstVisibleItemScrollOffset / 50f))
+
+    Log.d("mLog", "scrollOffset = ${scrollOffset}")
+    Log.d("mLog", "scrollState.firstVisibleItemIndex = ${scrollState.firstVisibleItemIndex}")
+    Log.d(
+        "mLog",
+        "scrollState.firstVisibleItemScrollOffset = ${scrollState.firstVisibleItemScrollOffset}"
+    )
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(crystal)
-            .padding(top = 24.dp)
-            .padding(horizontal = 24.dp), verticalArrangement = Arrangement.spacedBy(24.dp)
+            .padding(top = 24.dp),
     ) {
-
         BoxWithConstraints(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = gray1, shape = RoundedCornerShape(16.dp))
                 .aspectRatio(1f)
+                .shadow(elevation = scrollOffset.dp)
+                .padding(bottom = 24.dp)
+                .padding(horizontal = 24.dp)
+                .background(color = gray1, shape = RoundedCornerShape(16.dp))
                 .neu(
                     radius = 16.dp,
                     pressed = true,
@@ -67,7 +88,8 @@ fun PresentPictureScreen() {
 
             (painter.state as? AsyncImagePainter.State.Success)?.result?.drawable?.let {
                 AdaptiveImage(
-                    drawable = it, paddingCoefficient = 1.5
+                    drawable = it,
+                    paddingCoefficient = 1.5
                 )
             }
 
@@ -83,29 +105,38 @@ fun PresentPictureScreen() {
             BODY3Text(
                 modifier = Modifier
                     .align(Alignment.BottomEnd)
-                    .padding(end = 16.dp, bottom = 8.dp),
-                text = "by Max"
+                    .padding(end = 16.dp, bottom = 8.dp), text = sign
             )
         }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            state = scrollState,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
+            item {
 
-            H1Text(
-                text = "Van Gogh"
-            )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
 
-            RatingBar(
-                modifier = Modifier.height(20.dp),
-                rating = 2.7f,
-                colorEnabled = darkYellow,
-                colorDisabled = gray3
-            )
+                    H1Text(text = title)
+
+                    RatingBar(
+                        modifier = Modifier.height(20.dp),
+                        rating = 2.7f,
+                        colorEnabled = darkYellow,
+                        colorDisabled = gray3
+                    )
+                }
+
+                BODY2Text(text = description)
+            }
         }
-
-        BODY2Text(
-            text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-        )
     }
+}
+
+@Composable
+private fun Header() {
+
 }
